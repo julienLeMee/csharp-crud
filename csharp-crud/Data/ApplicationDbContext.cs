5326bd00822ca7805d592cebed092f0130605e77
Julien Le Mee <julemee@gmail.com>
2023-02-15 18:40:35 -0500

    starting project

---
+++ csharp-crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_crud.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
